b2762db5d383af045cee722460d763de387ec9f4
Carl Boije <boijec@gmail.com>
2025-06-14 15:39:16 +0200

    updating main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use job_execution;
-use persistence;
-use acme_client;
-use acme_client::crypto::SupportedKey;
-use acme_client::keys::PrivateKey;
+use std::collections::HashMap;
+use std::env;
+use common_utils::parsing::{InternalInlineParsingTool, ParamOption};
+use common_utils::parsing::ParamOption::{Option, Value};
 
 fn main() {
-    let pkey = PrivateKey::from_supported_type(SupportedKey::Rsa2048).unwrap();
-    let jwk = pkey.get_jwk().unwrap();
-    println!("{:?}", jwk);
+    let args: Vec<String> = env::args().collect();
+    let mut argument_iterator = args.iter();
+    // skip program name!
+    argument_iterator.next().unwrap();
+    let mut allowed_args: HashMap<String, ParamOption> = HashMap::new();
+    allowed_args.insert("--acme-base-url".to_string(), Value);
+    allowed_args.insert("--verbose".to_string(), Value);
+    allowed_args.insert("-f".to_string(), Option);
+    let map = InternalInlineParsingTool::parse_args(&mut argument_iterator, &allowed_args);
+    if map.is_err() {
+        eprintln!("{}", map.unwrap_err());
+        println!("show usage bumper - then die!");
+        return;
+    }
+    // handle the consequences of the map
+    // initiate call to acme ca server for directory fetch
+    // save directory to datastore
+    // create client / user - keys
+    println!("argument map: {:#?}", map);
 }
